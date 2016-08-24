9c4102f78d062eb2a7b2c6a1a2ee064389dd1495
dylan.parmley <dylan.parmley@parivedasolutions.com>
2016-08-24 14:42:56 -0500

    FolderMonitor Progress

---
+++ ConsoleApplication1/FolderMonitor.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Security.Permissions;
+
+
+namespace ConsoleApplication1
+{
+    public class FolderMonitor : IFolderMonitor
+    {
+        public string FolderName { get; set; }
+        public IStorageProvider StorageProvider { get; set; }
+        public FileSystemWatcher Watcher { get; set; }
+        public bool Running { get; set; }
+
+        public FolderMonitor(string folderName, IStorageProvider storageProvider)
+        {
+            Initialize(folderName, storageProvider);
+
+        }
+
+        public void Initialize(string folderName, IStorageProvider storageProvider)
+        {
+            FolderName = folderName;
+            StorageProvider =  storageProvider;
+
+            // Create a new FileSystemWatcher and set its properties.
+            Watcher = new FileSystemWatcher();
+            Watcher.Path = folderName;
+
+            Watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite
+               | NotifyFilters.FileName | NotifyFilters.DirectoryName; ;
+            // Only watch text files.
+            Watcher.Filter = "*.pdf";
+
+            // Add event handlers.
+            //Watcher.Changed += new FileSystemEventHandler(OnChanged);
+            Watcher.Created += new FileSystemEventHandler(OnChanged);
+        }
+
+        [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
+        public void Observe()
+        {
+            // Begin watching.
+            Watcher.EnableRaisingEvents = true;
+        }
+
+
+        //create a blob of the file added to the folder we are watching in azure
+        public void OnChanged(object source, FileSystemEventArgs e)
+        { 
+            StorageProvider.CreateBlob(FolderName, e.Name);
+        }
+       
+        
+
+    }
+}
---
+++ ConsoleApplication1/IFolderMonitor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApplication1
+{
+    public interface IFolderMonitor
+    {
+        //IFolderMonitor();
+        //IFolderMonitor(string folderName, IStorageProvider storageProvider);
+        string FolderName { get; set; }
+        IStorageProvider StorageProvider { get; set; }
+        FileSystemWatcher Watcher { get; set; }
+        bool Running { get; set; }
+
+        void Initialize(string folderName, IStorageProvider storageProvider);
+        void Observe();
+        void OnChanged(object source, FileSystemEventArgs e);
+
+
+
+
+
+    }
+}
